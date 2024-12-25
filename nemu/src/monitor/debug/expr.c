36a6e81b5d0c66347b4f65e2dac1070474743da0
tracer-ics2019 <tracer@njuics.org>
2024-12-25 23:21:48 +0800

    >  compile U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:21:48 up 21:09,  4 users,  load average: 0.00, 0.00, 0.00 f620eb539be52a72b2175a091d853fd8edc73e15

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -3,13 +3,23 @@
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
+#include <stdlib.h>
 #include <sys/types.h>
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
+  TK_AND,
+  TK_OR,
+  TK_NOT,
+  TK_NEQ,
+  TK_NUM,
+  TK_HEX_NUM,
+  TK_REG,
+  TK_NEG,
+  TK_POINT
 
 };
 
@@ -24,7 +34,19 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"==", TK_EQ},         // equal
+  {"\\*", '*'},         // multiply
+  {"\\/", '/'},         // divide
+  {"\\-", '-'},         // sub
+  {"\\(", '('},         // left bracket
+  {"\\)", ')'},         // right bracket
+  {"&&", TK_AND},       // and
+  {"\\|\\|", TK_OR},    // or
+  {"!=", TK_NEQ},       // not equal
+  {"0[xX][0-9a-fA-F]+", TK_HEX_NUM}, // hex_num
+  {"[0-9]+", TK_NUM},   // num
+  {"\\$[a-z]+[0-9]*", TK_REG},  // reg
+  {"!", TK_NOT},        // not
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -80,7 +102,48 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE:
+            break;
+          case TK_NUM:{
+            Assert(substr_len < 32, "TK_NUM: substr length too long!");
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str , substr_start , substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+            break;
+          }
+          case TK_HEX_NUM:{
+            Assert(substr_len < 34, "TK_HEX_NUM: substr length too long!");
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str , substr_start + 2 , substr_len - 2);
+            tokens[nr_token].str[substr_len - 2] = '\0';
+            nr_token++;
+            break;
+          }
+          case TK_REG:{
+            Assert(substr_len < 32, "TK_NUM: substr length too long!");
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str , substr_start + 1 , substr_len - 1);
+            tokens[nr_token].str[substr_len - 1] = '\0';
+            nr_token++;
+            break;
+          }
+          case '(':
+          case ')':
+          case '*':
+          case '/':
+          case '+':
+          case '-':
+          case TK_EQ:
+          case TK_AND:
+          case TK_OR:
+          case TK_NEQ:
+          case TK_NOT:
+          default:{
+            tokens[nr_token].type = rules[i].token_type;
+            nr_token++;
+            break;
+          }
         }
 
         break;
@@ -94,6 +157,166 @@ static bool make_token(char *e) {
   }
 
   return true;
+}
+
+bool check_parentheses(int start, int end){
+  if(tokens[start].type != '(' || tokens[end].type != ')'){
+    return false;
+  }
+  int res = 0;
+  for(int i = start+1 ; i < end ; i++){
+    if(tokens[i].type == '('){
+      res++;
+    }else if(tokens[i].type == ')'){
+      res--;
+    }
+
+    if(res < 0){
+      return false;
+    }
+  }
+  return res == 0;
+}
+
+int find_main_op(int start, int end){
+  int pos = -1;
+  int res = 0;
+  int priority = 0;
+
+  for(int i = start ; i <= end ; i++){
+    int cur_priority = -1;
+    if(tokens[i].type == '('){
+      res++;
+    }else if(tokens[i].type == ')'){
+      res--;
+    }
+    if(res != 0){
+      continue;
+    }
+
+    switch (tokens[i].type)
+    {
+    case TK_NOT:{
+      cur_priority = 2;
+      break;
+    }
+    case TK_NEG:{
+      cur_priority = 2;
+      break;
+    }
+    case TK_POINT:{
+      cur_priority = 2;
+    }
+    case '*':
+    case '/':{
+      cur_priority = 3;
+      break;
+    }
+    case '+':
+    case '-':{
+      cur_priority = 4;
+      break;
+    }
+    case TK_EQ:
+    case TK_NEQ:{
+      cur_priority = 7;
+      break;
+    }
+    case TK_AND:{
+      cur_priority = 11;
+      break;
+    }
+    case TK_OR:{
+      cur_priority = 12;
+      break;
+    }
+    default:
+      break;
+    }
+
+    if(priority <= cur_priority){
+      priority = cur_priority;
+      pos = i;
+    }
+  }
+
+  Assert((pos != -1 && priority != 0) , "cannot find main operator!");
+  return pos;
+}
+
+uint32_t eval(int start, int end, bool *success){
+  if(start > end){
+    printf("start > end , invalid expr!\n");
+  }
+
+  if(start == end){
+    switch (tokens[start].type)
+    {
+    case TK_NUM:{
+      return (uint32_t)strtol(tokens[start].str , NULL , 10);
+    }
+    case TK_HEX_NUM:{
+      return (uint32_t)strtol(tokens[start].str , NULL , 16);
+    }
+    case TK_REG:{
+      return isa_reg_str2val(tokens[start].str , success);
+    }
+    default:
+      printf("single token with no value!\n");
+      return 0;
+    }
+  }else if(check_parentheses(start , end)){
+    return eval(start+1 , end-1 , success);
+  }else {
+    int pos = find_main_op(start , end);
+
+    if(pos == start || tokens[pos].type == TK_NOT || tokens[pos].type == TK_POINT || tokens[pos].type == TK_NEG){
+      uint32_t r_val = eval(pos+1 , end , success);
+      switch (tokens[pos].type)
+      {
+      case TK_NOT:{
+        return !r_val;
+      }
+      case TK_NEG:{
+        return -r_val;
+      }
+      case TK_POINT:{
+        return paddr_read(r_val, 4);
+      }
+      default:
+        printf("invalid unary operator\n");
+        return 0;
+      }
+    }
+
+    uint32_t l_val = eval(start , pos-1 , success);
+    uint32_t r_val = eval(pos+1 , end , success);
+
+    switch (tokens[pos].type)
+    {
+    case '+': return l_val + r_val;
+    case '-': return l_val - r_val;
+    case '*': return l_val * r_val;
+    case '/': {
+      Assert(r_val != 0 , "Division by zero error!");
+      if(r_val == 0){
+        printf("Division by zero error!\n");
+        return 0;
+      }
+      return l_val / r_val;
+    }
+    case TK_EQ: return l_val == r_val;
+    case TK_NEQ: return l_val != r_val;
+    case TK_AND: return l_val && r_val;
+    case TK_OR: return l_val || r_val;
+
+    default:
+      printf("invalid operator!\n");
+      return 0;
+    }
+  }
+  *success = false;
+  return 0;
 }
 
 uint32_t expr(char *e, bool *success) {

73f38e0b43f2ec40bf933b438a439818d0af0ce7
tracer-ics2019 <tracer@njuics.org>
2024-12-26 22:26:13 +0800

    >  compile U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:26:13 up 1 day,  1:14,  4 users,  load average: 0.00, 0.00, 0.00 506e827c3e4dd2ebeb8b5372a88950b138d9b3aa

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,5 +14,10 @@ void isa_reg_display() {
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
+  for(int i = 0 ; i < 32 ; i++){
+    if(strcmp(s , regsl[i]) == 0){
+      return cpu.gpr[i]._32;
+    }
+  }
   return 0;
 }

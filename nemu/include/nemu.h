5166908d8ba3570bdcd073c491df007769717a2a
tracer-ics2019 <tracer@njuics.org>
2024-12-30 20:52:40 +0800

    >  compile U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:52:40 up  3:33,  3 users,  load average: 0.02, 0.07, 0.04 e90d7e30a1d8b4aef2bc5893908517dca0ef8ff

--- nemu/include/nemu.h
+++ nemu/include/nemu.h
@@ -3,7 +3,11 @@
 
 #include "common.h"
 #include "memory/memory.h"
+#ifdef RISCV
 #include "isa/reg.h"
+#else
+#include "isa/riscv32/include/isa/reg.h"
+#endif
 
 extern CPU_state cpu;
 
--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -1,7 +1,23 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
 
+const char *reg_sl[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void ref_isa_display(CPU_state *ref_r){
+  printf("-----ref regs-----\n");
+  for(int i = 0 ; i < 32 ; i++){
+    printf("%-4s      0x%-8x      %-8d\n", reg_sl[i] , ref_r->gpr[i]._32 , ref_r->gpr[i]._32);
+  }
+  printf("-----dut regs-----\n");
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  ref_isa_display(ref_r);
   return false;
 }
 

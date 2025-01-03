284d244e415f5265c06f8e20cf3285fd0dc3b7cd
tracer-ics2019 <tracer@njuics.org>
2025-01-03 17:34:44 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:34:44 up 15:20,  3 users,  load average: 0.00, 0.02, 0.00 946f639519541d6080ec92be8f381de0482826d2

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,16 +3,82 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int _i2a(int num, char buf[32], int radix){
+  static const char s[] = "0123456789abcdef";
+  char *dst = buf;
+  int n = num , R = radix;
+  if(n < 0){
+    *dst++ = '-';
+    n = -num;
+  } 
+  if(n < 10){
+    *dst++ = s[n];
+    *dst = '\0';
+  }else {
+    char tmp[32];
+    char *p = tmp;
+    while(n){
+      *p++ = s[n % R];
+      n = n / R;
+    }
+    while(p != tmp){
+      --p;
+      *dst++ = *p;
+    }
+    *dst = '\0';
+  }
+  return dst - buf;
+}
+
+int _itoa(int num, char buf[32]){
+  return _i2a(num, buf, 10);
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  size_t pos = 0;
+  int format = 0;
+
+  for(; *fmt ; fmt++){
+    if(format){
+      switch (*fmt)
+      {
+      case 'd':{
+        char buf[32];
+        int n = _itoa(va_arg(ap, int), buf);
+        memcpy(out+pos, buf, n);
+        pos += n;
+        break;
+      }
+      case 's':{
+        const char *s2 = va_arg(ap, char*);
+        int n = strlen(s2);
+        memcpy(out+pos, s2, n);
+        pos += n;
+        break;
+      }
+      default:
+        break;
+      }
+    }else if(*fmt == '%'){
+      format = 1;
+    }else {
+      out[pos++] = *fmt;
+    }
+  }
+  out[pos] = '\0';
+  return pos;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list vl;
+  va_start(vl, fmt);
+  int length = vsprintf(out, fmt, vl);
+  va_end(vl);
+  return length;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

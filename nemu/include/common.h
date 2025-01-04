e70acfa5ba72a4da514a29ff4c167f2d3f505eac
tracer-ics2019 <tracer@njuics.org>
2025-01-04 10:37:14 +0800

    >  compile U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:37:14 up 17:03,  3 users,  load average: 0.19, 0.23, 0.11 2206e9be3eb3dc374c94132161104ddaee53bfe3

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -3,7 +3,6 @@
 
 #define DEBUG
 #define DIFF_TEST
-#define HAS_IOE
 
 #if _SHARE
 // do not enable these features while building a reference design
@@ -12,7 +11,7 @@
 #endif
 
 /* You will define this macro in PA2 */
-//#define HAS_IOE
+#define HAS_IOE
 
 #include <stdint.h>
 #include <assert.h>

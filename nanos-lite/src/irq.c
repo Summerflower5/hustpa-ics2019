3b618729d19533ec059662c3938a263a2c58c9fd
tracer-ics2019 <tracer@njuics.org>
2025-01-05 18:28:27 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:28:27 up 1 day,  7:39,  3 users,  load average: 0.02, 0.06, 0.01 bcaabb3e711f5caab51f7ce435532844ec7adf

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,6 +6,10 @@ static _Context* do_event(_Event e, _Context* c) {
       printf("EVENT YIELD TRAP!\n");
       break;
     }
+    case _EVENT_SYSCALL:{
+      do_syscall(c);
+      break;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -13,6 +13,15 @@ _Context* __am_irq_handle(_Context *c) {
         ev.event = _EVENT_YIELD;
         break;
       }
+      case 0:  case 1:  case 2:  case 3:
+      case 4:  case 5:  case 6:  case 7:
+      case 8:  case 9:  case 10: case 11:
+      case 12: case 13: case 14: case 15:
+      case 16: case 17: case 18: case 19:{
+        ev.event = _EVENT_SYSCALL;
+        break;
+      }
+
       default: ev.event = _EVENT_ERROR; break;
     }
 

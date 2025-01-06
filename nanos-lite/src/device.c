e85211216b732c1be2fca9a3720faccf61ae181e
tracer-ics2019 <tracer@njuics.org>
2025-01-06 18:12:31 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:12:31 up 1 day, 15:15,  3 users,  load average: 0.19, 0.16, 0.11 dda0e9ccff0e5ece559363827c790a0e9eb38a2c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,6 +2,7 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  Log("serial_write is called!");
   for(int i = 0 ; i < len ; i++){
     _putc(((const char*)buf)[i]);
   }

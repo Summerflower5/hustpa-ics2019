49c3da71e2e42e4fe72d6ae5e2de155717b551a7
tracer-ics2019 <tracer@njuics.org>
2025-01-06 17:47:17 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:47:17 up 1 day, 14:50,  3 users,  load average: 0.16, 0.11, 0.07 7f25c4af1f61d24d84f52585e2bb5724a3026613

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,7 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
   assert(fd > 2 && fd < NR_FILES);
 
-  if(file_table[fd].open_offset + len > file_table[fd].size){
+  if(file_table[fd].open_offset + len >= file_table[fd].size){
     if(file_table[fd].open_offset < file_table[fd].size){
       len = file_table[fd].size - file_table[fd].open_offset;
     }else {

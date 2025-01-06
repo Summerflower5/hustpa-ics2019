a6db7054e613d80c8d9edfc6dcf4fcb964a832f6
tracer-ics2019 <tracer@njuics.org>
2025-01-06 12:11:14 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:11:14 up 1 day, 13:06,  3 users,  load average: 0.32, 0.08, 0.02 e77dd72a9de6d5f5c2e5154d25e8fba2b1673836

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -2,7 +2,7 @@
 
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
 extern size_t events_read(void *buf, size_t offset, size_t len);
-extern dispinfo_read(void *buf, size_t offset, size_t len);
+extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
 extern size_t fb_write(const void *buf, size_t offset, size_t len);
 extern size_t fbsync_write(const void *buf, size_t offset, size_t len);
 

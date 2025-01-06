531400178c695883c07de33e83f5fd24cfade563
tracer-ics2019 <tracer@njuics.org>
2025-01-06 18:06:00 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:06:00 up 1 day, 15:08,  3 users,  load average: 0.02, 0.08, 0.07 a15b99c9d824b91e49aed935eaa089111e1320f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -94,7 +94,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
         len = 0;
       }
     }
-    printf("fd %d write %d bytes\n",fd , len);
+    printf("fd %d write %d bytes at %d\n",fd , len, file_table[fd].disk_offset + file_table[fd].open_offset);
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }else {
     len = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);

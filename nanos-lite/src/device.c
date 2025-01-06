6f74ee6936c393aa4f7144f123bd589c5db55d94
tracer-ics2019 <tracer@njuics.org>
2025-01-06 17:44:55 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:44:55 up 1 day, 14:47,  3 users,  load average: 0.25, 0.09, 0.06 16bdd9a3deacb7c474d2b31b8ccb597d6b478f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,9 +35,9 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  memcpy(buf, dispinfo + offset , len);
-  //len = sprintf(buf, dispinfo + offset);
-  Log("dispinfo_read called here");
+  //memcpy(buf, dispinfo + offset , len);
+  len = sprintf(buf, dispinfo + offset);
+  //Log("dispinfo_read called here");
   return len;
 }
 
@@ -45,13 +45,13 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   uint32_t y = (offset / 4) / screen_width();
   uint32_t x = (offset / 4) % screen_width();
   draw_rect((uint32_t *)buf, x, y, len / 4 , 1);
-  Log("fb_write called here");
+  //Log("fb_write called here");
   return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
   draw_sync();
-  Log("fbsync_write called here");
+  //Log("fbsync_write called here");
   return 0;
 }
 
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,14 +64,14 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
   assert(fd > 2 && fd < NR_FILES);
 
-  if(file_table[fd].read == NULL){
-    if(file_table[fd].open_offset + len > file_table[fd].size){
-      if(file_table[fd].open_offset < file_table[fd].size){
-        len = file_table[fd].size - file_table[fd].open_offset;
-      }else {
-        len = 0;
-      }
+  if(file_table[fd].open_offset + len > file_table[fd].size){
+    if(file_table[fd].open_offset < file_table[fd].size){
+      len = file_table[fd].size - file_table[fd].open_offset;
+    }else {
+      len = 0;
     }
+  }
+  if(file_table[fd].read == NULL){
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }else {
     len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -86,14 +86,14 @@ size_t fs_write(int fd, const void *buf, size_t len){
   assert(fd >= 0);
   if(fd == 0) return len;
 
-  if(file_table[fd].write == NULL){
-    if(file_table[fd].open_offset + len > file_table[fd].size){
-      if(file_table[fd].open_offset < file_table[fd].size){
-        len = file_table[fd].size - file_table[fd].open_offset;
-      }else {
-        len = 0;
-      }
+  if(file_table[fd].open_offset + len > file_table[fd].size){
+    if(file_table[fd].open_offset < file_table[fd].size){
+      len = file_table[fd].size - file_table[fd].open_offset;
+    }else {
+      len = 0;
     }
+  }
+  if(file_table[fd].write == NULL){
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }else {
     len = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);

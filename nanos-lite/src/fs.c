b0f3c1691b8c9adcc57bb52cb5ba6dc8f235aec2
tracer-ics2019 <tracer@njuics.org>
2025-01-06 11:42:54 +0800

    >  run U202115529 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:42:54 up 1 day, 12:38,  3 users,  load average: 0.22, 0.07, 0.02 1431bb738910b191967fcab4693ddcbd96c2a82e

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,6 +1,7 @@
 #include "fs.h"
 
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
@@ -31,6 +32,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/events", 0, 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
